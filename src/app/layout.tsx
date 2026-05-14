b1daed2ab4f686ed620af87d959acb576f3a99ef
Adildrouz <adil.drouz@gmail.com>
2026-05-14 12:14:45 +0100

    Add Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,20 @@
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return children;
+  return (
+    <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-BWN7CLQ0VX"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-BWN7CLQ0VX');
+            `,
+          }}
+        />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
 }
